17907b7ab58682ac915a7d8281a7951ecfbfd84c
tukauch-ops <tukauch@gmail.com>
2026-01-12 16:29:50 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/ttsapp/MainActivity.kt
+++ app/src/main/java/com/example/ttsapp/MainActivity.kt
@@ -4,9 +4,8 @@ import android.app.Activity
 import android.os.Bundle
 
 class MainActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView を呼ばない
     }
 }
