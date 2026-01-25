193607b5cb9b5bce7ad1f7a82f6cc838af4625f2
tukauch-ops <tukauch@gmail.com>
2026-01-25 15:23:05 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/ttsapp/MainActivity.kt
+++ app/src/main/java/com/example/ttsapp/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.example.ttsapp
+package com.example.ttsapp2
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val tv = TextView(this)
-        tv.text = "起動テスト成功"
+        tv.text = "HELLO 起動成功"
         tv.textSize = 24f
 
         setContentView(tv)
