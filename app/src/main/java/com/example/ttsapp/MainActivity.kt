fb9a29e717c0a7877abf11fd9e05a47f15899c6d
tukauch-ops <tukauch@gmail.com>
2026-01-27 16:38:04 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/ttsapp/MainActivity.kt
+++ app/src/main/java/com/example/ttsapp/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.ttsapp2
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Toast.makeText(this, "HELLO 起動成功", Toast.LENGTH_LONG).show()
-
-        setContentView(android.R.layout.simple_list_item_1)
+        setContentView(R.layout.activity_main)
     }
 }
