b537b6f255727193643557616671c342e629901f
tukauch-ops <tukauch@gmail.com>
2026-01-31 16:41:27 +0900

    Create FloatingButtonService.kt

---
+++ app/src/main/java/com/example/ttsapp/FloatingButtonService.kt
@@ -0,0 +1,13 @@
+package com.example.ttsapp
+
+import android.app.Service
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.IBinder
+import android.provider.Settings
+import android.view.*
+import android.widget.ImageView
+
+class FloatingButtonService : Service() {
+    ...
+}
